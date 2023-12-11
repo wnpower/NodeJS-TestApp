e588a9845f32b9277de91723da433732f96e7acb
Ignacio Morandin <imorandin@wnpower.com>
2023-12-11 14:31:00 -0300

    add more output

--- test_app.js
+++ test_app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
-app.get('/', function (req, res) {
-	  res.send('Hello World!');
+app.get('/*', function (req, res) {
+	  res.send('¡Estoy andando!, node version: ' + process.versions.node +' , ruta pedida: ' + req.url);
 });
 app.listen(3000, function () {
 	  console.log('Aplicación de pruebas escuchando en el puerto 3000!');
