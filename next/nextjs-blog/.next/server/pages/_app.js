a51f4740e4cfbff78c5e514552c0aa3500c75a29
Ignacio Morandin <imorandin@wnpower.com>
2024-01-12 11:40:19 -0300

    add build

---
+++ next/nextjs-blog/.next/server/pages/_app.js
@@ -0,0 +1,1 @@
+(()=>{var r={};r.id=888,r.ids=[888],r.modules={2166:(r,e,s)=>{"use strict";s.r(e),s.d(e,{default:()=>u});var t=s(997);function u({Component:r,pageProps:e}){return t.jsx(r,{...e})}s(9605)},9605:()=>{},997:r=>{"use strict";r.exports=require("react/jsx-runtime")}};var e=require("../webpack-runtime.js");e.C(r);var s=e(e.s=2166);module.exports=s})();
