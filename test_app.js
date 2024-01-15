4283c842d68e6de05bf9517467d88d46390be179
Ignacio Morandin <imorandin@wnpower.com>
2024-01-15 17:22:47 -0300

    show node_env

--- test_app.js
+++ test_app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 app.get('/*', function (req, res) {
-	  res.send('¡Estoy andando!, node version: ' + process.versions.node +' , ruta pedida: ' + req.url);
+	  res.send('¡Estoy andando!, node version: ' + process.versions.node + ' , ruta pedida: ' + req.url + ', NODE_ENV: ' + process.env.NODE_ENV);
 });
 app.listen(3000, function () {
 	  console.log('Aplicación de pruebas escuchando en el puerto 3000!');
